4d828e4212d71801fdc00f3f51afaa5e4e2018b5
agraunak45 <126402615+agraunak45@users.noreply.github.com>
2024-03-14 23:20:13 +0530

    Update hello.py

--- pythontask/hello.py
+++ pythontask/hello.py
@@ -1,1 +1,22 @@
-hi
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
+        
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
